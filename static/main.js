97af372f8227afe076d94f160c8dfa6e66748715
Raul Pinero <52712445+RPinero-20@users.noreply.github.com>
2022-04-25 10:08:59 -0400

    Add files via upload

---
+++ static/main.js
@@ -0,0 +1,7 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.menu').classList.toggle('show');
+});
+
+/* function closeNav(){
+    document.querySelector('.nav-bar').classList.remove('show');
+} */
